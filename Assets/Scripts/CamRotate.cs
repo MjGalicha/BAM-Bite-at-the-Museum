4f29784965394b98a5b90a2fe58ccbd2db7a6c3f
MjGalicha <mjgalicha@gmail.com>
2020-10-01 17:20:41 -0600

    imported stuff from old github

---
+++ Assets/Scripts/CamRotate.cs
@@ -0,0 +1,28 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CamRotate : MonoBehaviour
+{
+    public float rotateLeft;
+    public float rotateRight;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("q"))
+        {
+            Debug.Log("key pressed");
+            GetComponent<CinemachineFreeLook>().m_XAxis.Value += rotateLeft * Time.deltaTime;
+        }
+
+        if (Input.GetKey("e"))
+        {
+            Debug.Log("e key pressed");
+            GetComponent<CinemachineFreeLook>().m_XAxis.Value += rotateRight * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Grab.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    private bool bitten = false;
+    private GameObject grabbedItem;
+    public Animator anim;
+    private float offset = -5.0f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("v") && bitten)
+            Drop(grabbedItem);
+
+        if (Input.GetKeyDown("c") && bitten)
+            StartCoroutine(Eat(grabbedItem));
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Bite") && Input.GetKey("b") && !bitten)
+        {
+            grabbedItem = col.gameObject;
+            Pickup(grabbedItem);
+        }
+    }
+
+    public void Pickup(GameObject Obj)
+    {
+        Obj.transform.position = this.transform.position;
+        Obj.transform.position = Obj.transform.position + new Vector3(0, offset, 0);
+        Obj.transform.parent = this.transform;
+        Obj.transform.GetComponent<Rigidbody>().isKinematic = true;
+        bitten = true;
+        if (Obj.GetComponent<Navigation_Reversal_Script>() != null)
+        {
+            Obj.GetComponent<Navigation_Reversal_Script>().enabled = false;
+        }
+    }
+
+    public void Drop(GameObject Obj)
+    {
+        Obj.transform.parent = null;
+        Obj.transform.GetComponent<Rigidbody>().isKinematic = false;
+        bitten = false;
+        anim.SetTrigger("Bite");
+        grabbedItem = null;
+    }
+
+    IEnumerator Eat(GameObject Obj)
+    {
+        anim.SetTrigger("Bite");
+        yield return new WaitForSeconds(0.5f);
+        Destroy(Obj);
+        grabbedItem = null;
+        bitten = false;
+    }
+}
